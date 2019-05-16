352c5e2db736b158fef1edc2b6c1fddc62a4f8a1
Hetty <lidiyasysueva@gmail.com>
2019-05-16 10:41:20 +0400

    v2

--- src/main/java/com/nc/training/center/chat/controllers/AccountController.java
+++ src/main/java/com/nc/training/center/chat/controllers/AccountController.java
@@ -4,10 +4,9 @@ import com.nc.training.center.chat.domains.Role;
 import com.nc.training.center.chat.domains.User;
 import com.nc.training.center.chat.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.Map;
@@ -22,7 +21,7 @@ public class AccountController {
         return "registration";
     }
 
-    @PostMapping("/registration")
+   /* @PostMapping("/registration")
     public String addUser(User user, Map<String, Object> model) {
         try {
 
@@ -34,6 +33,28 @@ public class AccountController {
             System.out.println(e.getMessage());
         }
         return "redirect:/login.html";
+
+    }*/
+
+
+    @RequestMapping(value = {"/registration"}, method = RequestMethod.POST)
+    public String addUser(
+            @RequestParam("login") String login,
+
+            @RequestParam("password") String pass ){
+        try {
+            User user = new User();
+            user.setLogin(login);
+            user.setPassword(new BCryptPasswordEncoder().encode(pass));
+            user.setActive(true);
+           // user.setRoles(Collections.singleton(Role.USER));
+            userRepository.save(user);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return "redirect:/login.html";
+
 
     }
 }
